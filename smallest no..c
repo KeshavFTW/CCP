41531bdc130498214850ab49136ab5908cd998be
Keshav Somani <83907243+KeshavFTW@users.noreply.github.com>
2021-06-07 10:30:22 +0530

    Add files via upload

---
+++ smallest no..c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+
+    int a,b,c;
+    printf("enter the values of a b c: \n");
+    scanf("%d %d %d",&a,&b,&c);
+
+    if(a<b && a<c){
+        printf("the smallest no. is %d\n",a);
+
+    }
+
+    else if(b<c && b<a){
+        printf("the samllest no. is %d\n",b);
+    }
+    else {
+        printf("the smallest no. is %d\n",c);
+    }
+    return 0;
+}
