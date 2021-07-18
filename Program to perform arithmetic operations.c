830ef25677014a88ccaf499a62f32fae0dd825a7
Keshav Somani <83907243+KeshavFTW@users.noreply.github.com>
2021-07-18 22:50:00 +0530

    Add files via upload

---
+++ Program to perform arithmetic operations.c
@@ -0,0 +1,29 @@
+// 1. Develop a C program to perform arithmetic operations (addition, subtraction, multiplication, divisionand remainder) on
+// two integers using pointers.
+#include<stdio.h>
+int ops(int , int *, int *, int *, int, float *, int *);
+int main()
+{
+     int a,b;
+     int add,sub,mul,rem;
+     float quo;
+     printf("Enter num 1 : ");
+     scanf("%d",&a);
+     printf("Enter num 2 : ");
+     scanf("%d",&b);
+     ops(&a, &b, &add, &sub, &mul, &quo, &rem);
+     printf("\n");
+     printf("Sum        :%d\n",add);
+     printf("Difference :%d\n",sub);
+     printf("Quotient   :%0.2f\n",quo);
+     printf("Product    :%d\n",mul);
+     printf("Remainder  :%d\n",rem);
+}
+intops(int *a, int *b, int *add, int *sub, int *mul, float *quo, int *rem)
+{
+     *add=*a+*b;
+     *sub=*a-*b;
+     mul=a*b;
+     *quo=(float)(*a)/(*b);
+     *rem=(*a)%(*b);
+}
